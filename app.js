3361f1ee432fac052a9b6960d51d8852abf92ac5
samuel-oso <ososamuel035@gmail.com>
2022-09-02 03:01:30 +0100

    integrating prismic with project

--- app.js
+++ app.js
@@ -1,27 +1,79 @@
+require("dotenv").config();
+
+console.log(process.env.PRISMIC_ENDPOINT, process.env.PRISMIC_CLIENT_ID);
+
 const express = require("express");
+
 const app = express();
 const path = require("path");
 const port = 3000;
 
+const Prismic = require("@prismicio/client");
+const PrismicDOM = require("prismic-dom");
+
+const initApi = (req) => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req,
+  });
+};
+
+const handleLinkResolver = (doc) => {
+  // // Define the url depending on the document type
+  // if (doc.type === "page") {
+  //   return "/page/" + doc.uid;
+  // } else if (doc.type === "blog_post") {
+  //   return "/blog/" + doc.uid;
+  // }
+
+  // Default to homepage
+  return "/";
+};
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver,
+  };
+
+  res.locals.PrismicDOM = PrismicDOM;
+
+  next();
+});
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.render("pages/home");
 });
 
 app.get("/about", (req, res) => {
-  res.render("pages/about");
+  initApi(req).then((api) => {
+    api
+      .query(Prismic.Predicates.any("document.type", ["about", "meta"]))
+      .then((response) => {
+        const { results } = response;
+        const [about, meta] = results;
+
+        console.log(about, meta);
+
+        res.render("pages/about", {
+          about,
+          meta,
+        });
+      });
+  });
 });
 
-app.get("/collections", (req, res) => {
-  res.render("pages/collections");
+app.get("/collection", async (req, res) => {
+  res.render("pages/collection");
 });
 
-app.get("/detail/:id", (req, res) => {
+app.get("/detail/:uid", async (req, res) => {
   res.render("pages/detail");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
