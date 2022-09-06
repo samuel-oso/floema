5da13520a7ee2a43983ed3d67a62362c005ead0c
samuel-oso <ososamuel035@gmail.com>
2022-09-06 17:38:29 +0100

    Implementation of semantic HTMl with Pug, Prismic and Express

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 require("dotenv").config();
 
-console.log(process.env.PRISMIC_ENDPOINT, process.env.PRISMIC_CLIENT_ID);
-
+const logger = require("morgan");
 const express = require("express");
+const errorHandler = require("errorhandler");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 const app = express();
 const path = require("path");
 const port = 3000;
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride());
+app.use(errorHandler());
 
 const Prismic = require("@prismicio/client");
 const PrismicDOM = require("prismic-dom");
@@ -19,21 +27,36 @@ const initApi = (req) => {
 };
 
 const handleLinkResolver = (doc) => {
-  // // Define the url depending on the document type
-  // if (doc.type === "page") {
-  //   return "/page/" + doc.uid;
-  // } else if (doc.type === "blog_post") {
-  //   return "/blog/" + doc.uid;
-  // }
+  if (doc.type === "product") {
+    return `detail/${doc.slug}`;
+  }
 
-  // Default to homepage
+  if (doc.type === "collections") {
+    return "/collections";
+  }
+
+  if (doc.type === "about") {
+    return "/about";
+  }
+
   return "/";
 };
 
+app.use(errorHandler());
+
 app.use((req, res, next) => {
-  res.locals.ctx = {
-    endpoint: process.env.PRISMIC_ENDPOINT,
-    linkResolver: handleLinkResolver,
+  res.locals.Link = handleLinkResolver;
+
+  res.locals.Numbers = (index) => {
+    return index == 0
+      ? "One"
+      : index == 1
+      ? "Two"
+      : index == 2
+      ? "Three"
+      : index == 3
+      ? "Four"
+      : "";
   };
 
   res.locals.PrismicDOM = PrismicDOM;
@@ -44,34 +67,79 @@ app.use((req, res, next) => {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+const handleRequest = async (api) => {
+  const meta = await api.getSingle("meta");
+  const navigation = await api.getSingle("navigation");
+  const preloader = await api.getSingle("preloader");
+
+  return {
+    meta,
+    navigation,
+    preloader,
+  };
+};
+
 app.get("/", async (req, res) => {
-  res.render("pages/home");
-});
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+  const home = await api.getSingle("home");
 
-app.get("/about", (req, res) => {
-  initApi(req).then((api) => {
-    api
-      .query(Prismic.Predicates.any("document.type", ["about", "meta"]))
-      .then((response) => {
-        const { results } = response;
-        const [about, meta] = results;
+  const { results: collections } = await api.query(
+    Prismic.Predicates.at("document.type", "collection"),
+    {
+      fetchLinks: "product.image",
+    }
+  );
 
-        console.log(about, meta);
-
-        res.render("pages/about", {
-          about,
-          meta,
-        });
-      });
+  res.render("pages/home", {
+    ...defaults,
+    collections,
+    home,
   });
 });
 
-app.get("/collection", async (req, res) => {
-  res.render("pages/collection");
+app.get("/about", async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+  const about = await api.getSingle("about");
+
+  res.render("pages/about", {
+    ...defaults,
+    about,
+  });
+});
+
+app.get("/collections", async (req, res) => {
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+  const home = await api.getSingle("home");
+
+  const { results: collections } = await api.query(
+    Prismic.Predicates.at("document.type", "collection"),
+    {
+      fetchLinks: "product.image",
+    }
+  );
+
+  res.render("pages/collections", {
+    ...defaults,
+    collections,
+    home,
+  });
 });
 
 app.get("/detail/:uid", async (req, res) => {
-  res.render("pages/detail");
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+
+  const product = await api.getByUID("product", req.params.uid, {
+    fetchLinks: "collection.title",
+  });
+
+  res.render("pages/detail", {
+    ...defaults,
+    product,
+  });
 });
 
 app.listen(port, () => {
