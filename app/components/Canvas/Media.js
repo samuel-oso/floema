788f0d51e6bce76ed1de88e7d64da45f5f4cd382
samuel-oso <ososamuel035@gmail.com>
2022-10-02 22:54:52 +0100

    Implementing a draggable view with WebGL

--- app/components/Canvas/Media.js
+++ app/components/Canvas/Media.js
@@ -1,32 +1,28 @@
 import { Mesh, Program, Texture } from "ogl";
-
 import vertex from "../../shaders/plane-vertex.glsl";
 import fragment from "../../shaders/plane-fragment.glsl";
 
 export default class Media {
-  constructor({ element, geometry, gl, index, scene }) {
+  constructor({ element, geometry, gl, index, scene, sizes }) {
     this.element = element;
     this.gl = gl;
     this.geometry = geometry;
     this.scene = scene;
     this.index = index;
+    this.sizes = sizes;
 
     this.createTexture();
     this.createProgram();
     this.createMesh();
   }
-
   createTexture() {
     this.texture = new Texture(this.gl);
-
-    console.log(this.element);
 
     this.image = new window.Image();
     this.image.crossOrigin = "anonymous";
     this.image.src = this.element.getAttribute("data-src");
     this.image.onload = (_) => (this.texture.image = this.image);
   }
-
   createProgram() {
     this.program = new Program(this.gl, {
       fragment,
@@ -36,7 +32,6 @@ export default class Media {
       },
     });
   }
-
   createMesh() {
     this.mesh = new Mesh(this.gl, {
       geometry: this.geometry,
@@ -45,6 +40,49 @@ export default class Media {
 
     this.mesh.setParent(this.scene);
 
-    this.mesh.position.x = this.index * this.mesh.scale.x;
+    this.mesh.scale.x = 2;
+  }
+
+  createBounds({ sizes }) {
+    this.sizes = sizes;
+    this.bounds = this.element.getBoundingClientRect();
+
+    this.updateScale();
+    this.updateX();
+    this.updateY();
+  }
+
+  // Events
+
+  onResize(sizes) {
+    this.createBounds(sizes);
+  }
+
+  // Loop.
+
+  updateScale() {
+    this.height = this.bounds.height / window.innerHeight;
+    this.width = this.bounds.width / window.innerWidth;
+
+    this.mesh.scale.x = this.sizes.width * this.width;
+    this.mesh.scale.x = this.sizes.height * this.width;
+  }
+
+  updateX(x = 0) {
+    this.x = (this.bounds.left + x) / window.innerWidth;
+
+    this.mesh.position.x = (-this.sizes.width / 2) + (this.mesh.scale.x / 2) + (this.x  * this.sizes.width); // prettier-ignore
+  }
+
+  updateY(y = 0) {
+    this.y = (this.bounds.top + y) / window.innerHeight;
+
+    this.mesh.position.y = (this.sizes.height / 2) - (this.mesh.scale.y / 2) - (this.y  * this.sizes.height); // prettier-ignore
+  }
+
+  update(scroll) {
+    if (!this.bounds) return;
+    this.updateX(scroll.x);
+    this.updateY(scroll.y);
   }
 }
